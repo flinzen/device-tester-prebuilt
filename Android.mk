7ca7c2af403f5b32065ec5122432a5e29ff1d845
Dmitry Shchapin <diman199806@gmail.com>
2022-08-18 17:22:24 +0500

    initial commit

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := DeviceTester
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES := DeviceTester.apk
+include $(BUILD_PREBUILT)
